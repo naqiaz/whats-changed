6290fbba7fce995e79138b1162e38ce5c69154bf
David Tran <trandavidq@gmail.com>
2021-02-27 02:04:36 -0500

    Added barebones authentication w/o Google OAuth2. TODO: Change out HttpResponse objects

--- changed/urls.py
+++ changed/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'changed'
 urlpatterns = [
     path('',views.index,name='index'),
+    path('signup/register/',views.registerUser,name='register'),
+    path('signup/',views.signup,name='signup'),
+    path('authenticate',views.auth,name='authenticate')
 ]
--- changed/views.py
+++ changed/views.py
@@ -1,6 +1,51 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def index(request):
-    return render(request,'changed/index.html')
+    #the default page is the login page
+    return render(request,'changed/login.html')
+
+def auth(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    print(username)
+    print(password)
+
+    verify = authenticate(username=username,password=password)
+    if verify is not None:
+        #The user is in the database, log them in
+        login(request,verify)
+        print("Logged in successfully")
+    else:
+        #The user is not in the database. Register them by creating a new user and adding to the database
+        user = User.objects.create_user(username=username,password=password)
+
+    return HttpResponse("asdasdasd")
+
+def signup(request):
+    return render(request,'changed/signup.html')
+def registerUser(request):
+    '''
+    registerUser goes hand-in-hand with signup. registerUser captures info from signup and adds the user to the database,
+    then logs them in.
+    '''
+
+    #TODO: Render an actual template instead of HTTPResponse
+    username = request.POST['username']
+    password = request.POST['password']
+    print(username)
+    print(password)
+
+    verify = authenticate(username=username,password=password)
+    if verify is not None:
+        #The user is in the database, log them in
+        print("asd")
+    else:
+        #The user is not in the database. Register them by creating a new user and adding to the database
+        user = User.objects.create_user(username=username,password=password)
+
+
+    return HttpResponse("testingtesting123")
