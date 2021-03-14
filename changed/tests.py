8c172544c1dd4d4e5e1fdf9a9954051f15c7d0e2
David Tran <trandavidq@gmail.com>
2021-03-13 22:18:16 -0500

    Wrote login test

--- changed/tests.py
+++ changed/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout as auth_logout
 
 # Create your tests here.
+class AuthTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='TestUsername',password= 'TestPassword')
+
+
+
+    def test_login(self):
+        #test_user = User.objects.create(username="TestUsername", password = "TestPassword")
+        user = User.objects.get(username='TestUsername')
+        data = {
+            'username': user.username,
+            'password': user.password,
+        }
+        response = self.client.post(reverse('changed:authenticate'),data)
+        self.assertTrue(user.is_authenticated)
+        
