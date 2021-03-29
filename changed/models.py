2fed5ef4e40cbd65ea9671d713d2f7bbd41c3b4f
naqiaz <62780042+naqiaz@users.noreply.github.com>
2021-03-29 13:23:18 -0400

    Update models.py

--- changed/models.py
+++ changed/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 
 class Business(models.Model):
     business_name = models.TextField()
-    business_addr = models.TextField(null=False,default="No address")
     category = models.CharField(max_length = 50)
     def __str__(self):
         return self.business_name
