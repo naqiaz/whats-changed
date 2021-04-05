785079f6e7eaa25a41c7bd7ba195125f62a22eb0
naqiaz <62780042+naqiaz@users.noreply.github.com>
2021-04-04 20:01:10 -0400

    Update 0003_auto_20210404_0949.py

--- changed/migrations/0003_auto_20210404_0949.py
+++ changed/migrations/0003_auto_20210404_0949.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('changed', '0002_business_business_addr'),
+        ('changed', '0001_initial'),
     ]
 
     operations = [
