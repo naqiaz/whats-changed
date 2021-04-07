551d11a4168bee44260dfd0c6f6b729b74bcacac
naqiaz <ntz3kq@virginia.edu>
2021-04-07 18:10:53 -0400

    Store information from write review

--- changed/views.py
+++ changed/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout as auth_logout
 from .models import Business, BusinessInfo
+from .models import BusinessForm
 from django.views.generic import DetailView
 
 # Create your views here.
 def index(request):
     if request.user.is_authenticated:
-            return render(request,'changed/home.html')
+            form = BusinessForm()
+            return render(request,'changed/home.html',{'form':form})
     else:
             return render(request,'changed/login.html')
 
@@ -20,30 +23,52 @@ def writeReview(request):
     '''
     TODO: Business = business_name, BusinessInfo = covid_compliance_rating
     '''
-    review_text = request.POST['review-text']
-    business_name=request.POST['businessName']
-    business_pid=request.POST['businessPid']
-    print(business_name)
     user = request.user
-    if(Business.objects.filter(business_name=business_name,business_pid=business_pid).exists()):
-        #the business already exists, don't create a duplicate
-        print('The business already exists')
-        business = Business.objects.get(business_name=business_name,business_pid=business_pid)
-        user = User.objects.get(username=request.user.username)
-        business_info = BusinessInfo.objects.create(business=business,user=user, body = review_text) 
-        print(business_name)
-        print(user.username)
-        print(review_text)
-        return redirect('changed:index')
+    if request.method == 'POST':
+        form = BusinessForm(request.POST)
+        if form.is_valid():
+            business_name = request.POST['businessName']
+            business_pid = request.POST['businessPid']
+           # print(business_name)
+           # print(business_pid)
+            covid_compliance_rating = form.cleaned_data['covid_compliance_rating']
+           # print(covid_compliance_rating)
+            capacity_limit = form.cleaned_data['capacity_limit']
+           # print(capacity_limit)
+            indoor_dining = form.cleaned_data['indoor_dining']
+           # print(indoor_dining)
+            outdoor_dining = form.cleaned_data['outdoor_dining']
+           # print(outdoor_dining)
+            curbside_pickup = form.cleaned_data['curbside_pickup']
+           # print(curbside_pickup)
+            delivery = form.cleaned_data['delivery']
+           # print(delivery)
+            body = form.cleaned_data['body']
+           # print(body)
+            if(Business.objects.filter(business_name=business_name,business_pid=business_pid).exists()):
+            #the business already exists, don't create a duplicate
+                print('The business already exists')
+                business = Business.objects.get(business_name=business_name,business_pid=business_pid)
+                user = User.objects.get(username=request.user.username)
+                business_info = BusinessInfo.objects.create(business=business,user=user, covid_compliance_rating=covid_compliance_rating,capacity_limit=capacity_limit,
+                                                           indoor_dining=indoor_dining,outdoor_dining=outdoor_dining, curbside_pickup=curbside_pickup,delivery=delivery,body = body) 
+                print(business_name)
+                print(user.username)
+                print(body)
+                return HttpResponseRedirect(reverse('changed:index'))
+            else:
+                business = Business.objects.create(business_name=business_name,business_pid=business_pid)
+                user = User.objects.get(username=request.user.username)
+                business_info = BusinessInfo.objects.create(business=business,user=user, covid_compliance_rating=covid_compliance_rating,capacity_limit=capacity_limit,
+                                                           indoor_dining=indoor_dining,outdoor_dining=outdoor_dining, curbside_pickup=curbside_pickup,delivery=delivery,body = body)
+                print(business_name)
+                print(user.username)
+                print(review_text)
+                return HttpResponseRedirect(reverse('changed:index'))
+        return HttpResponseRedirect(reverse('changed:index'))
     else:
-        business = Business.objects.create(business_name=business_name,business_pid=business_pid)
-        user = User.objects.get(username=request.user.username)
-        business_info = BusinessInfo.objects.create(business=business, user=user,body = review_text)
-        print(business_name)
-        print(user.username)
-        print(review_text)
-        return redirect('changed:index')
-    
+        form = BusinessForm()
+    return render(request,'changed/home.html',{'form':form})
 
 def show_reviews(request):
     #this page shows the respective business and all of its comments, shown when "Read review" is clicked
@@ -61,7 +86,8 @@ def show_reviews(request):
             }
             return render(request,'changed/comments.html',context)
         else:
-            return render(request,'changed/home.html')
+            form = BusinessForm()
+            return render(request,'changed/home.html',{'form':form})
 
 def profile(request,username):
     #this page shows the respective profile and all of the user's reviews, when "profile" is clicked
@@ -76,7 +102,8 @@ def profile(request,username):
             }
             return render(request,'changed/profile.html',context)
         else:
-            return render(request,'changed/home.html')   
+            form = BusinessForm()
+            return render(request,'changed/home.html',{'form':form})   
 
     
     
