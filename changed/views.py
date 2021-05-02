4a153783e7ef2416e82a64c88c29e567545293a6
naqiaz <ntz3kq@virginia.edu>
2021-05-02 15:22:19 -0400

    Changed capacity limit field & about page accessibility

--- changed/views.py
+++ changed/views.py
@@ -10,18 +10,24 @@ import datetime
 import pytz
 from django.http import JsonResponse
 
-
-
-# Create your views here.
 def index(request):
+    # if the user is authenticated, render the home page 
     if request.user.is_authenticated:
             form = BusinessForm()
             return render(request,'changed/home.html',{'form':form})
+    # if the user is not authenticated, render the login page
     else:
             form = BusinessForm()
             return render(request,'changed/login.html',{'form':form})
 
 def rating(request):
+    ################################################################################################################
+    # REFERENCE
+    # Title: How to Work with Ajax Request with Django
+    # Author: Vitor Freitas
+    # URL: https://simpleisbetterthancomplex.com/tutorial/2016/08/29/how-to-work-with-ajax-request-with-django.html
+    # Django License: BSD-3
+    ################################################################################################################
     name = request.GET['name']
     pid = request.GET['pid']
     if (Business.objects.filter(business_name=name,business_pid=pid).exists()):
@@ -51,18 +57,17 @@ def rating(request):
     return JsonResponse(data)
 
 def about(request):
-    if request.user.is_authenticated:
-            return render(request,'changed/about.html')
-    else:
-            form = BusinessForm()
-            return render(request,'changed/login.html',{'form':form})
+    # renders the view for an about page, which can be viewed even if a user is not authenticated
+      return render(request,'changed/about.html')
 
 
 def signup(request):
+    # renders the view for a sign up page, with a form
     return render(request,'changed/signup.html')
 
 
 def writeReview(request):
+    # handles the submission of a business review
     if request.method == 'POST':
         form = BusinessForm(request.POST)
         if form.is_valid():
@@ -76,8 +81,9 @@ def writeReview(request):
             delivery = form.cleaned_data['delivery']
             body = form.cleaned_data['body']
             published_date = pytz.utc.localize(datetime.datetime.utcnow())
+
+            # if the business exists do not create a duplicate
             if(Business.objects.filter(business_name=business_name,business_pid=business_pid).exists()):
-            #the business already exists, don't create a duplicate
                 business = Business.objects.get(business_name=business_name,business_pid=business_pid)
                 user = User.objects.get(username=request.user.username)
                 business_info = BusinessInfo.objects.create(business=business,user=user, covid_compliance_rating=covid_compliance_rating,capacity_limit=capacity_limit,
@@ -85,6 +91,8 @@ def writeReview(request):
                                                            published_date = published_date)
                 business.average()
                 return HttpResponseRedirect(reverse('changed:index'))
+
+            # else create a new business object and a comment object
             else:
                 business = Business.objects.create(business_name=business_name,business_pid=business_pid,average_rating=0)
                 user = User.objects.get(username=request.user.username)
@@ -100,9 +108,10 @@ def writeReview(request):
 
 
 def show_reviews(request):
-    #this page shows the respective business and all of its comments, shown when "Read review" is clicked
+    # this page shows the respective business and all of its comments, shown when "Read review" is clicked
         business_name = request.POST['businessName']
         business_pid = request.POST['businessPid']
+
         if (Business.objects.filter(business_name=business_name,business_pid=business_pid).exists()):
             business = Business.objects.get(business_name=business_name,business_pid=business_pid)
             avg_rating = business.average_rating
@@ -123,11 +132,12 @@ def show_reviews(request):
             return render(request,'changed/home.html',{'form':form})
 
 def profile(request,username):
-    #this page shows the respective profile and all of the user's reviews, when "profile" is clicked
+    # this page shows the respective profile and all of the user's reviews, when "profile" is clicked
         if (User.objects.filter(username=username).exists()):
             user = User.objects.get(username=username)
             username = user.username
             business_info = user.businessinfo_set.all().order_by('-published_date')
+            replies = user.reply_set.all().order_by('-published_date')
             context = {
             'user':user,
             'user_reviews':business_info,
@@ -138,6 +148,7 @@ def profile(request,username):
             return render(request,'changed/home.html',{'form':form})  
 
 def reply(request,id):
+    # this page shows all the replies to a specific comment, and handles the submission of new replies
         if (BusinessInfo.objects.filter(id=id).exists()):
             comment = BusinessInfo.objects.get(id=id)
             replies = comment.reply_set.all().order_by('-published_date') 
@@ -149,13 +160,13 @@ def reply(request,id):
             'form':form,
             'editForm':editForm,
             }
+            # if a reply is submitted create a new reply object
             if request.method == 'POST':
               form = ReplyForm(request.POST)
               if form.is_valid():
                 reply = form.cleaned_data['reply']
                 user = request.user
                 published_date = pytz.utc.localize(datetime.datetime.utcnow())
-                
                 reply = Reply.objects.create(body=reply,comment=comment,user=user,published_date=published_date)
             return render(request,'changed/replies.html',context)   
         else:
@@ -163,6 +174,7 @@ def reply(request,id):
             return render(request,'changed/home.html',{'form':form})
 
 def delete_reply(request,comment_id, reply_id):
+    # handles a request to delete a reply object
         if (Reply.objects.filter(id=reply_id).exists()):
             reply = Reply.objects.filter(id=reply_id).delete()
         comment = BusinessInfo.objects.get(id=comment_id)
@@ -178,6 +190,7 @@ def delete_reply(request,comment_id, reply_id):
         return render(request,'changed/replies.html',context)
 
 def edit_reply(request,comment_id, reply_id):
+    # handles a request to update a reply object
     reply = Reply.objects.get(id=reply_id)
     comment = BusinessInfo.objects.get(id=comment_id)
     replies = comment.reply_set.all().order_by('-published_date')
@@ -197,6 +210,7 @@ def edit_reply(request,comment_id, reply_id):
     return render(request,'changed/replies.html',context)
 
 def delete_comment(request,business_pid,comment_id):
+    # handles a request to delete a comment object
     if (BusinessInfo.objects.filter(id=comment_id).exists()):
         BusinessInfo.objects.filter(id=comment_id).delete()
     business = Business.objects.get(business_pid=business_pid)
@@ -212,6 +226,7 @@ def delete_comment(request,business_pid,comment_id):
     return render(request,'changed/comments.html',context)
 
 def edit_comment(request,business_pid,comment_id):
+    # handles a request to update a comment object
     comment = BusinessInfo.objects.get(id=comment_id)
     business = Business.objects.get(business_pid=business_pid)
     business_info = business.businessinfo_set.all().order_by('-published_date') 
